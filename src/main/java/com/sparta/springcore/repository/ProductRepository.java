409b39a2861ad2677039ebaa28046afed27db6b1
Minkyu222341 <qkralsrb2040@naver.com>
2022-07-28 15:44:17 +0900

    스프링 심화 1주차

---
+++ src/main/java/com/sparta/springcore/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.sparta.springcore.repository;
+
+import com.sparta.springcore.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> { }
